88663100d29eb7498ef3d707d14a26d267e93fe5
Monoget <monoget2@gmail.com>
2023-12-05 16:15:59 +0600

    update all

--- js/mapmarker_func.jquery.js
+++ js/mapmarker_func.jquery.js
@@ -1,6 +1,6 @@
 		$('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
 		//set up markers 
-		var myMarkers = {"markers": [
+		/*var myMarkers = {"markers": [
 				{"latitude": "51.511732", "longitude":"-0.123270", "icon": "img/map-marker.png"}
 			]
 		};
@@ -10,6 +10,6 @@
 			zoom	: 14,
 			center	: 'Covent Garden London',
 			markers	: myMarkers
-		});
+		});*/
 
 });
